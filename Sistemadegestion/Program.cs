30b76c814e12cc23c1fcbd58c20cfecc352f18f0
GASTON ORREGO <90807963+gastonorrego@users.noreply.github.com>
2022-08-15 15:40:56 -0300

    Add files via upload

---
+++ Sistemadegestion/Program.cs
@@ -0,0 +1,27 @@
+﻿using productHandler;
+
+
+
+
+    public class ProbarObjetos
+{
+
+    static void Main(string[] args)
+    {
+      
+
+        UsuarioClaveHandler usuarioClaveHandler = new UsuarioClaveHandler();
+        usuarioClaveHandler.GetUsuarioClave();
+
+
+        UsuarioHandler usuarioHandler = new UsuarioHandler();
+        usuarioHandler.GetUsuario();
+
+        ProductoHandler productoHandler = new ProductoHandler();
+        productoHandler.GetProducto();
+
+        ProductoVendidoHandler productoVendidoHandler = new ProductoVendidoHandler();
+        productoVendidoHandler.GetProductoVendido();
+
+    }
+}
